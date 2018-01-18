9557bafa77a80cc9bb3c22aa1e05ea70b4a34f3b
danielmmy <danielmmy10@gmail.com>
2018-01-18 09:37:07 -0200

    Using local gf-complete library instead of dynamic linked

--- gen_VGF_AES.sh
+++ gen_VGF_AES.sh
@@ -1,1 +1,1 @@
-gcc -L/usr/local/lib/libgf_complete VGF_AES.c -lgf_complete -o VGF_AES
+gcc VGF_AES.c gf_complete.c gf_w4.c gf_cpu.c gf_wgen.c gf_w8.c gf_w16.c gf_w32.c gf_w64.c gf_w128.c -o VGF_AES
