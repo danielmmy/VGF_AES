a6597bb681a8f46d374c4ffab7bbc47129442a6e
danielmmy <danielmmy10@gmail.com>
2018-01-31 12:39:31 -0200

    Added Sbox for gf6

--- VGF_AES.c
+++ VGF_AES.c
@@ -33,15 +33,49 @@ unsigned int gf8_at_multiply(uint8_t num, uint8_t S){
 	return retval;
 }
 
+
+unsigned int gf6_at_multiply(uint8_t num, uint8_t S){
+        int i=0;
+	int j=0;
+        uint8_t counter;
+        uint8_t retval=0;
+        uint8_t tmp;
+        for(i=5;i>=0;--i){
+                tmp=S&num;
+                counter=0;
+                for(j=0;j<6;++j){
+                        counter+=tmp%2;
+                        tmp>>=1;
+                }
+                if(counter%2){
+                        retval|=1UL<<i;
+                }
+
+
+
+                //TODO need to fix using logical shift
+                if(S%2){
+                        S>>=1;
+                        S|=32;
+                }else{
+                        S>>=1;
+                }
+                //ENDTODO
+        }
+        return retval&63;
+}
+
+
 int main(int argc, char **argv){
-        int w=8;
 	int i,n;
         gf_t gf;
 	uint8_t inv, mult, add;
 
         /* Create the proper instance of the gf_t object using the polynomial x^8+x^4+x^3+x+1: */
-        gf_init_hard(&gf, w, GF_MULT_DEFAULT, GF_REGION_DEFAULT, GF_DIVIDE_DEFAULT,
+        gf_init_hard(&gf, 8, GF_MULT_DEFAULT, GF_REGION_DEFAULT, GF_DIVIDE_DEFAULT,
                       0x11b, 0, 0,NULL, NULL);
+
+	printf("S-BOX and inverted S-BOX for GF X^8+X^4+X^3+X+1");
 
 	for(i=0;i<256;++i){
 		if(i%16==0){
@@ -71,7 +105,32 @@ int main(int argc, char **argv){
                         printf("%2x ",inv);
                 }
         }
+//	printf("\n99 inv %d\n99X211=%d\n",gf.inverse.w32(&gf, 99),gf.multiply.w32(&gf,99,211));
         printf("\n\n");
+
+	gf_init_hard(&gf, 6, GF_MULT_DEFAULT, GF_REGION_DEFAULT, GF_DIVIDE_DEFAULT,
+                      0x43, 0, 0,NULL, NULL);
+	
+	printf("S-BOX and inverted S-BOX for GF X^6+X+1");
+
+        for(i=0;i<64;++i){
+                if(i%8==0){
+                        printf("\n");
+                }
+                if(i==0){
+                        printf("%2x ",38);
+                }else{
+                        inv=gf.inverse.w32(&gf, i);
+                        mult=gf6_at_multiply(inv,37);
+                        add=mult^38;
+                        printf("%2x ",add);
+                }
+        }
+//	printf("\n37 inv %d\n37X9=%d\n",gf.inverse.w32(&gf, 37),gf.multiply.w32(&gf,37,9));
+        printf("\n\n");
+
+        printf("\n\n");
+
 
 }
 
